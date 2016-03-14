435469451c274e78fd309356afba4e957566d248
joamag <joamag@gmail.com>
2016-03-14 23:55:00 +0000

    multiple kwargs in apps

--- src/examples/app.py
+++ src/examples/app.py
@@ -43,8 +43,12 @@ from . import base
 
 class TututmApp(appier.WebApp):
 
-    def __init__(self):
-        appier.WebApp.__init__(self, name = "tutum")
+    def __init__(self, *args, **kwargs):
+        appier.WebApp.__init__(
+            self,
+            name = "tutum",
+            *args, **kwargs
+        )
 
     @appier.route("/", "GET")
     def index(self):
