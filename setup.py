1fcdc09af104acee9134764059b703db8e630775
joamag <joamag@gmail.com>
2017-05-16 12:54:29 +0100

    new setup file and link

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setuptools.setup(
     description = "Tutum API Client",
     license = "Apache License, Version 2.0",
     keywords = "tutum api",
-    url = "http://tutum_api.hive.pt",
+    url = "http://tutum-api.hive.pt",
     zip_safe = False,
     packages = [
         "tutum"
