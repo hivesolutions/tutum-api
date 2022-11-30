e624a2056e785075ff1c324ae1526d93faf70c93
João Magalhães <joamag@gmail.com>
2022-11-30 16:53:53 +0000

    feat: renamed repo

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ import os
 import setuptools
 
 setuptools.setup(
-    name = "tutum_api",
+    name = "tutum-api",
     version = "0.1.1",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
